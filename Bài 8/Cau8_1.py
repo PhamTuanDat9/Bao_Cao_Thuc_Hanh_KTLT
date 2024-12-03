20504cb5e173f431fbe650d2f199ba87cc1d693a
PhamTuanDat9 <datpt52@gmail.com>
2024-12-03 20:57:20 +0700

    Cau8_1.py

---
+++ Bài 8/Cau8_1.py
@@ -0,0 +1,17 @@
+print("Pham Tuan Dat")
+print("235752021610105")
+import turtle
+window = turtle.Screen()
+window.bgcolor("lightgreen")
+painter = turtle.Turtle()
+painter.fillcolor('blue')
+painter.pencolor('blue')
+painter.pensize(3)
+def drawsq(t, s):
+    for i in range(4):
+        t.forward(s)
+        t.left(90)
+for i in range(1, 180):
+    painter.left(18)  
+    drawsq(painter, 200) 
+window.mainloop()
