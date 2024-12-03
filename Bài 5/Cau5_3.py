bab05e940de8db45b32ad5c72bd4034a110c4e42
PhamTuanDat9 <datpt52@gmail.com>
2024-12-03 21:16:45 +0700

    Add files via upload

---
+++ Bài 5/Cau5_3.py
@@ -0,0 +1,6 @@
+print("Pham Tuan Dat")
+print("235752021610105")
+import numpy as np
+x = np.arange(12, 38)
+print(x)
+
---
+++ Bài 5/Cau5_4.py
@@ -0,0 +1,7 @@
+print("Pham Tuan Dat")
+print("235752021610105")
+array = list(range(12, 38))
+print("Array created:", array)
+reversed_array = array[::-1]
+print("Reversed array:", reversed_array)
+
