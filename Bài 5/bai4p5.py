a6f7ac1f6e606310ed51d5d0e0799f8769520038
PhamTuanDat9 <datpt52@gmail.com>
2024-12-03 21:11:32 +0700

    Delete Bài 5/bai4p5.py

--- Bài 5/bai4p5.py
+++
@@ -1,7 +0,0 @@
-print("Pham Tuan Dat")
-print("235752021610105")
-array = list(range(12, 38))
-print("Array created:", array)
-reversed_array = array[::-1]
-print("Reversed array:", reversed_array)
-
