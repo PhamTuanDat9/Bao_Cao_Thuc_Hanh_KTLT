1c7c2e1db3a10cd3e3e4469b8b1c1188c0464c61
PhamTuanDat9 <datpt52@gmail.com>
2024-12-03 21:12:17 +0700

    Cau5_7.py

---
+++ Bài 5/Cau5_7.py
@@ -0,0 +1,15 @@
+print("Pham Tuan Dat")
+print("235752021610105")
+import numpy as np
+data_type = [('name', 'S15'), ('class', int), ('height', float)]
+students_details = [
+    ('James', 5, 48.5),
+    ('Nail', 6, 52.5),
+    ('Paul', 5, 42.10),
+    ('Pit', 5, 40.11)
+]
+students = np.array(students_details, dtype=data_type)
+print("Original array:")
+print(students)
+print("\nSort by height:")
+print(np.sort(students, order='height'))
